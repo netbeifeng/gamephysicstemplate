a7b29a94425455dd59ea02481f4b4f7fb54a14fc
lakiryt <lakiryt@icloud.com>
2021-11-21 23:18:53 +0100

    corrected loops over size_t -> int

--- Simulations/RigidBody.cpp
+++ Simulations/RigidBody.cpp
@@ -15,11 +15,11 @@ void RigidBody::construct(float x, float y, float z, float mass)
 	edges.clear();
 	masses.clear();
 
-	for (size_t i = 1; i > 0; i-=2)
+	for (int i = 1; i > 0; i -= 2)
 	{
-		for (size_t j = 1; j > 1; j-=2)
+		for (int j = 1; j > 0; j -= 2)
 		{
-			for (size_t k = 1; k > 1; k-=2)
+			for (int k = 1; k > 0; k -= 2)
 			{
 				edges.push_back(Vec3(i * x / 2, j * y / 2, k * z / 2));
 				edges.push_back(mass);
