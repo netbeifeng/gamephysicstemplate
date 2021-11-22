a9515111c988c298e4ce5459eeb11fbe6d6fa74d
lakiryt <lakiryt@icloud.com>
2021-11-22 01:14:00 +0100

    Renamed/added fields and its initializations

--- Simulations/RigidBody.cpp
+++ Simulations/RigidBody.cpp
@@ -2,7 +2,7 @@
 
 RigidBody::RigidBody(float x, float y, float z, float mass)
 {
-	edges.clear();
+	o_edges.clear();
 	masses.clear();
 
 	for (int i = 1; i > 0; i -= 2)
@@ -11,11 +11,25 @@ RigidBody::RigidBody(float x, float y, float z, float mass)
 		{
 			for (int k = 1; k > 0; k -= 2)
 			{
-				edges.push_back(Vec3(i * x / 2, j * y / 2, k * z / 2));
-				edges.push_back(mass);
+				o_edges.push_back(Vec3(i * x / 2, j * y / 2, k * z / 2));
+				masses.push_back(mass);
 			}
 		}
 	}
 
-	centerOfMass = Vec3(0, 0, 0);
+	w_centerOfMass = Vec3(0, 0, 0);
+	w_centerVelocity = Vec3(0, 0, 0);
+	w_to_o_orientation = Quat(1,0,0,0);
+
+	float mx = mass * 8 * (x / 2) * (x / 2);
+	float my = mass * 8 * (y / 2) * (y / 2);
+	float mz = mass * 8 * (z / 2) * (z / 2);
+	
+	o_inertiaTensorInv = Mat4(1/(my + mz), 0,    0   , 0,
+								0, 1/(mx + mz), 0   , 0,
+								0,    0, 1/(mx + my), 0,
+								0,    0,    0   , 0);
+
+	o_torque = Vec3(0, 0, 0);
+	o_linForce = Vec3(0, 0, 0);
 }
--- Simulations/RigidBody.h
+++ Simulations/RigidBody.h
@@ -12,9 +12,15 @@ class RigidBody
 public:
 	RigidBody(float x=1,float y=1,float z=1, float m=1);
 
-private:
 
-	vector<Vec3> edges;
+private:
+	vector<Vec3> o_edges;
 	vector<float> masses;
-	Vec3 centerOfMass;
+	Vec3 w_centerOfMass;
+	Vec3 w_centerVelocity;
+	Quat w_to_o_orientation;
+	Vec3 angularMomentum;
+	Mat4 o_inertiaTensorInv;
+	Vec3 o_torque;
+	Vec3 o_linForce;
 };
